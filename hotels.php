7305d85202b16d9d442947c09c58c3f3e856a77f
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-03-10 18:38:32 +0100

    qualche esperimento

--- hotels.php
+++ hotels.php
@@ -39,3 +39,34 @@
         ],
 
     ];
+
+    // $array_prova = [
+    //   'nome' => 'gianni',
+    //   'cognome' => 'carli',
+    // ];
+    //  var_dump(array_key_exists('nome',$array_prova));
+    //  $array_prova = [
+    //   'nome' => 'gianni',
+    //  'cognome' => 'carli',
+    //  ];
+    //  var_dump(in_array('neselli',$array_prova));
+    ?>
+   
+   
+    
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+    </head>
+    <body>
+      <ul>
+        
+       <li></li>
+      </ul>
+    </body>
+    </html>
